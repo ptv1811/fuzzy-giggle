abcf35ef582bdd8eb517fd8e6c7fa03472222ccc
An Ha <32453570+bannnn511@users.noreply.github.com>
2020-09-23 21:29:24 +0700

    add multi network

---
+++ Assets/Script/Launcher.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine;
+
+public class Launcher : MonoBehaviourPunCallbacks {
+
+    public GameObject playerModel;
+
+    public GameObject[] SpawnPoints;
+    string gameVersion = "1";
+
+    private void Awake () {
+        // this make sure PhotonNetwork.LoadLevel() on master and all clients in the same room sync their level automatically
+        PhotonNetwork.AutomaticallySyncScene = true;
+    }
+
+    // Start is called before the first frame update
+    void Start () {
+        Connect ();
+        // Init ();
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+    }
+    void Init () {
+        if (playerModel == null) { } else {
+            // we're in a room. spawn a character for the local player. it gets synced by using PhotonNetwork.Instantiate
+            PhotonNetwork.Instantiate (this.playerModel.name, new Vector3 (-9.09f, 5.21f, 26.59f), Quaternion.identity, 0);
+        }
+    }
+    public void Connect () {
+        if (PhotonNetwork.IsConnected) {
+            PhotonNetwork.JoinRandomRoom ();
+        } else {
+            PhotonNetwork.ConnectUsingSettings ();
+            PhotonNetwork.GameVersion = gameVersion;
+        }
+    }
+
+    public override void OnConnectedToMaster () {
+        // PhotonNetwork.JoinOrCreateRoom ("Test", new RoomOptions { IsOpen = true, MaxPlayers = 4 }, TypedLobby.Default);
+        PhotonNetwork.JoinRandomRoom ();
+    }
+
+    public override void OnJoinedRoom () {
+        Debug.Log ("Now this client is in a room.");
+        Debug.Log ("Number of players:" + PhotonNetwork.LocalPlayer.ActorNumber);
+
+        if (Player.LocalPlayerInstance == null) {
+            PhotonNetwork.Instantiate (this.playerModel.name, new Vector3 (-9.09f, 5.21f, 26.59f), Quaternion.identity, 0);
+        } else {
+            // PhotonNetwork.Instantiate (this.playerModel.name, new Vector3 (-9.09f, 5.21f, 26.59f), Quaternion.identity, 0);
+            // PhotonNetwork.Instantiate (this.playerModel.name, SpawnPoints[PhotonNetwork.LocalPlayer.ActorNumber].transform.GetChild (0).position, Quaternion.identity, 0);
+        }
+
+    }
+    public override void OnJoinRandomFailed (short returnCode, string message) {
+        PhotonNetwork.CreateRoom ("game", new RoomOptions ());
+    }
+}
--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -1,21 +1,29 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using Photon.Pun;
+using Photon.Realtime;
 using UnityEngine;
 
-[RequireComponent(typeof(Rigidbody))]
-public class Player : MonoBehaviour
-{
+[RequireComponent (typeof (Rigidbody))]
+public class Player : MonoBehaviourPun {
+
+    [Tooltip ("The local player instance. Use this to know if the local player is represented in the Scene")]
+    public static GameObject LocalPlayerInstance;
     [System.Serializable]
-    public enum PlayerState
-    {
-        Grounded, Ragdoll, Falling, Jumping, Diving, DiveToGround
+    public enum PlayerState {
+        Grounded,
+        Ragdoll,
+        Falling,
+        Jumping,
+        Diving,
+        DiveToGround
     }
 
     public PlayerState mState = PlayerState.Grounded;
-    public Transform transformCam;
+    // public Transform transformCam;
 
-    [Header("Movement Speed")]
+    [Header ("Movement Speed")]
     public float Speed = 5f;
     public float diveSpeed = 3f;
     public float jumpSpeed = 5f;
@@ -26,7 +34,6 @@ public class Player : MonoBehaviour
     public KeyCode jumpKey = KeyCode.Space;
     public KeyCode diveKey = KeyCode.C;
     public LayerMask mask;
-
 
     #region Private Variables
     private Rigidbody playerRigid;
@@ -46,75 +53,81 @@ public class Player : MonoBehaviour
     Vector3 moveDirection;
     #endregion
 
-    private void Start()
-    {
-        playerRigid = GetComponent<Rigidbody>();
-        capsuleCollider = GetComponent<CapsuleCollider>();
-        anim = GetComponent<Animator>();
-        Ragdoll = GetComponent<RagdollSystem>();
+    private void Awake () {
+        if (photonView.IsMine) {
+            Player.LocalPlayerInstance = this.gameObject;
+        }
 
+        DontDestroyOnLoad (this.gameObject);
+    }
+    private void Start () {
+        playerRigid = GetComponent<Rigidbody> ();
+        capsuleCollider = GetComponent<CapsuleCollider> ();
+        anim = GetComponent<Animator> ();
+        Ragdoll = GetComponent<RagdollSystem> ();
+        CameraWork ();
     }
 
-    private void Update()
-    {
-        InputHandle();
-        CheckGround();
-        AnimatorController();
+    private void Update () {
+        if (photonView.IsMine == false && PhotonNetwork.IsConnected == true) {
+            return;
+        }
+        InputHandle ();
+        CheckGround ();
+        AnimatorController ();
         //inputH = Input.GetAxisRaw("Horizontal");
         //inputV = Input.GetAxisRaw("Vertical");
     }
+    void CameraWork () {
+        CameraWork _cameraWork = this.gameObject.GetComponent<CameraWork> ();
 
-    private void AnimatorController()
-    {
+        if (_cameraWork != null) {
+            if (photonView.IsMine) {
+                _cameraWork.OnStartFollowing ();
+            }
+        }
+    }
+    private void AnimatorController () {
         Vector3 _velocity = moveDirection;
-        _velocity = Vector3.Lerp(recentMoveVelocity, _velocity, 0.8f);
+        _velocity = Vector3.Lerp (recentMoveVelocity, _velocity, 0.8f);
         recentMoveVelocity = _velocity;
-        anim.SetFloat("HorizontalSpeed", _velocity.magnitude);
-        Debug.Log(_velocity.magnitude);
-        anim.SetBool("inDive", inDive);
-        anim.SetBool("isGrounded", mState == PlayerState.Grounded);
+        anim.SetFloat ("HorizontalSpeed", _velocity.magnitude);
+
+        anim.SetBool ("inDive", inDive);
+        anim.SetBool ("isGrounded", mState == PlayerState.Grounded);
     }
     float radius = 0.3f;
 
-
-    private void CheckGround()
-    {
+    private void CheckGround () {
         groundDistance = mState == PlayerState.Grounded ? 1.4f : 0.95f;
-        isGrounded = (Physics.SphereCast(transform.position + transform.up,
+        isGrounded = (Physics.SphereCast (transform.position + transform.up,
             radius, -transform.up, out hit, groundDistance, mask, QueryTriggerInteraction.Ignore));
     }
 
-    private void FixedUpdate()
-    {
-        if (mState == PlayerState.Ragdoll)
-        {
+    private void FixedUpdate () {
+        if (mState == PlayerState.Ragdoll) {
             currentYposition = transform.eulerAngles.y;
             momentum = Vector3.zero;
             return;
         }
-        HandleState();
+        HandleState ();
 
         //Vector3 velocity = Time.deltaTime * Speed * InputDirection();
         //playerRigid.MovePosition(transform.position + velocity);
     }
 
-    void HandleAction()
-    {
-        if (mState == PlayerState.Grounded)
-        {
-            if (jumpInput && !inJump)
-            {
+    void HandleAction () {
+        if (mState == PlayerState.Grounded) {
+            if (jumpInput && !inJump) {
                 mState = PlayerState.Jumping;
                 jumpCounter = 0.2f;
                 inJump = true;
-                anim.CrossFadeInFixedTime("Jump", 0.01f);
+                anim.CrossFadeInFixedTime ("Jump", 0.01f);
 
             }
         }
-        if (mState != PlayerState.Diving)
-        {
-            if (diveInput && !inDive)
-            {
+        if (mState != PlayerState.Diving) {
+            if (diveInput && !inDive) {
                 diveCounter = 0.2f;
                 inDive = true;
                 //anim.CrossFadeInFixedTime("Dive", 0.01f);
@@ -123,21 +136,17 @@ public class Player : MonoBehaviour
         }
     }
 
-    private void HandleState()
-    {
-        HandleAction();
-        switch (mState)
-        {
+    private void HandleState () {
+        HandleAction ();
+        switch (mState) {
             case PlayerState.Grounded:
-                Vector3 velocity = Time.deltaTime * Speed * InputDirection();
-                playerRigid.MovePosition(transform.position + velocity);
+                Vector3 velocity = Time.deltaTime * Speed * InputDirection ();
+                playerRigid.MovePosition (transform.position + velocity);
                 momentum = velocity;
-                if (!isGrounded)
-                {
+                if (!isGrounded) {
                     mState = PlayerState.Falling;
                 }
-                if (inDive)
-                {
+                if (inDive) {
                     //play sound later
                 }
                 inJump = false;
@@ -145,14 +154,13 @@ public class Player : MonoBehaviour
                 break;
             case PlayerState.Falling:
                 momentum = Vector3.down * gravity;
-                playerRigid.AddForce(momentum);
+                playerRigid.AddForce (momentum);
                 if (isGrounded)
                     mState = PlayerState.Grounded;
                 break;
             case PlayerState.Jumping:
                 jumpCounter -= Time.deltaTime;
-                if (jumpCounter <= 0)
-                {
+                if (jumpCounter <= 0) {
                     jumpCounter = 0;
                     mState = PlayerState.Falling;
                 }
@@ -162,8 +170,7 @@ public class Player : MonoBehaviour
                 break;
             case PlayerState.Diving:
                 diveCounter -= Time.deltaTime;
-                if (diveCounter <= 0)
-                {
+                if (diveCounter <= 0) {
                     diveCounter = 0;
                     mState = PlayerState.Falling;
                 }
@@ -177,44 +184,39 @@ public class Player : MonoBehaviour
         }
     }
 
-    private void InputHandle()
-    {
-        inputH = Input.GetAxisRaw("Horizontal");
-        inputV = Input.GetAxisRaw("Vertical");
-        jumpInput = Input.GetKey(jumpKey);
-        diveInput = Input.GetKeyDown(diveKey);
-        moveDirection = InputDirection();
+    private void InputHandle () {
+        inputH = Input.GetAxisRaw ("Horizontal");
+        inputV = Input.GetAxisRaw ("Vertical");
+        jumpInput = Input.GetKey (jumpKey);
+        diveInput = Input.GetKeyDown (diveKey);
+        moveDirection = InputDirection ();
     }
 
-    private void LateUpdate()
-    {
+    private void LateUpdate () {
         if (!inDive && mState != PlayerState.Ragdoll)
-            Rotation();
+            Rotation ();
     }
 
-    Vector3 InputDirection()
-    {
+    Vector3 InputDirection () {
         Vector3 _direction = Vector3.zero;
-        _direction += transformCam.forward * inputV;
-        _direction += transformCam.right * inputH;
+        _direction += Vector3.forward * inputV;
+        _direction += Vector3.right * inputH;
         if (_direction.magnitude > 1f)
-            _direction.Normalize();
+            _direction.Normalize ();
         _direction.y = 0f;
 
         return _direction;
     }
 
-    private void Rotation()
-    {
-        Vector3 moveDirection = InputDirection();
-        if (moveDirection.magnitude >= 0.001f)
-        {
+    private void Rotation () {
+        Vector3 moveDirection = InputDirection ();
+        if (moveDirection.magnitude >= 0.001f) {
             Vector3 _currentFoward = transform.forward;
-            float _angle = Vector3.Angle(_currentFoward, moveDirection);
-            float _sign = Mathf.Sign(Vector3.Dot(transform.up, Vector3.Cross(_currentFoward, moveDirection)));
+            float _angle = Vector3.Angle (_currentFoward, moveDirection);
+            float _sign = Mathf.Sign (Vector3.Dot (transform.up, Vector3.Cross (_currentFoward, moveDirection)));
             float _difference = _angle * _sign;
-            float _factor = Mathf.InverseLerp(0f, 90f, Mathf.Abs(_difference));
-            float _step = Mathf.Sign(_difference) * _factor * Time.deltaTime * turnSpeed;
+            float _factor = Mathf.InverseLerp (0f, 90f, Mathf.Abs (_difference));
+            float _step = Mathf.Sign (_difference) * _factor * Time.deltaTime * turnSpeed;
             if (_difference < 0f && _step < _difference)
                 _step = _difference;
             else if (_difference > 0f && _step > _difference)
@@ -225,6 +227,6 @@ public class Player : MonoBehaviour
             if (currentYposition < -360f)
                 currentYposition += 360f;
         }
-        transform.rotation = Quaternion.Euler(0f, currentYposition, 0f);
+        transform.rotation = Quaternion.Euler (0f, currentYposition, 0f);
     }
 }
