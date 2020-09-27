82dd0bc2d0aaa907fb2d845f73204f8ea5c37ad1
hieune <hieuminhnguyen95@gmail.com>
2020-09-27 23:45:41 +0700

    random

--- Assets/Scenes/Menu/Play.cs
+++ Assets/Scenes/Menu/Play.cs
@@ -7,8 +7,14 @@ public class Play : MonoBehaviour
 {
     public GameObject obj;
  public void PlayGame(){
+   System.Random rnd = new System.Random();
+int num  = rnd.Next(1, 100);
     PassMat.passmat(obj.GetComponent<Renderer>().material);
-     SceneManager.LoadScene(1);
+    if(num % 2 ==0){
+       num = 2;
+    }
+    else num =1;
+     SceneManager.LoadScene(num);
       }
    public void Git(){
       Application.OpenURL("https://github.com/ptv1811/fuzzy-giggle");
