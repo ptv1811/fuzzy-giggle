aafe27c2ea63a5d97e4286cea1410179aa3a9c05
An Ha <32453570+bannnn511@users.noreply.github.com>
2020-09-27 23:34:09 +0700

    update level 2

--- Assets/Scenes/Menu/Play.cs
+++ Assets/Scenes/Menu/Play.cs
@@ -7,27 +7,10 @@ public class Play : MonoBehaviour
 {
     public GameObject obj;
  public void PlayGame(){
-    FindObjectOfType<AudioScript>().PlaySound("Cancel");
     PassMat.passmat(obj.GetComponent<Renderer>().material);
      SceneManager.LoadScene(1);
-     
       }
    public void Git(){
-      FindObjectOfType<AudioScript>().PlaySound("Accept");
       Application.OpenURL("https://github.com/ptv1811/fuzzy-giggle");
-      
-   }
-   public void PlayCancel(){
-    
-       FindObjectOfType<AudioScript>().PlaySound("Cancel");
-   }
-   public void PlayAccept(){
-  FindObjectOfType<AudioScript>().PlaySound("Accept");
-   }
-   public void PlayPopUp(){
-      FindObjectOfType<AudioScript>().PlaySound("PopUp");
-   }
-   public void QuitGame(){
-Application.Quit();
    }
 }
