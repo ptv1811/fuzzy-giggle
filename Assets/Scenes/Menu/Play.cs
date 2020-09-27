559d94b5fbf171e45713d7680e69f944e96ce6d2
hieune <hieuminhnguyen95@gmail.com>
2020-09-28 00:07:43 +0700

    adjust

--- Assets/Scenes/Menu/Play.cs
+++ Assets/Scenes/Menu/Play.cs
@@ -14,9 +14,24 @@ int num  = rnd.Next(1, 100);
        num = 2;
     }
     else num =1;
+    FindObjectOfType<AudioScript>().PlaySound("Cancel");
      SceneManager.LoadScene(1);
       }
    public void Git(){
+      FindObjectOfType<AudioScript>().PlaySound("Accept");
       Application.OpenURL("https://github.com/ptv1811/fuzzy-giggle");
+   }
+    public void PlayCancel(){
+    
+       FindObjectOfType<AudioScript>().PlaySound("Cancel");
+   }
+   public void PlayAccept(){
+  FindObjectOfType<AudioScript>().PlaySound("Accept");
+   }
+   public void PlayPopUp(){
+      FindObjectOfType<AudioScript>().PlaySound("PopUp");
+   }
+   public void QuitGame(){
+      Application.Quit();
    }
 }
