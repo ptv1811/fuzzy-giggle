1bd284217a8a72d28f744804a3e96f25061bf817
hieune <hieuminhnguyen95@gmail.com>
2020-09-27 22:07:02 +0700

    add running sounds

---
+++ Assets/Photon/PhotonUnityNetworking/Resources/PlaySFX.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine.Audio;
+using UnityEngine;
+using System;
+
+public class PlaySFX : MonoBehaviour
+{
+    public Sound[] sounds;
+    void Awake()
+    {
+        foreach (Sound s in sounds)
+        {
+            s.audioSrc = gameObject.AddComponent<AudioSource>();
+            s.audioSrc.clip = s.clip;
+            s.audioSrc.volume = s.volume;
+            s.audioSrc.pitch = s.pitch;
+        }
+    }
+
+    // Update is called once per frame
+    public void PlaySound(string name)
+    {
+      
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        s.audioSrc.Play();
+        
+    }
+    public void Run(){
+        PlaySound("Running");
+    }
+    public void StopSound(string name)
+    {
+
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+      
+        s.audioSrc.Stop();
+    }
+}
